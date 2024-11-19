39a8fe4e133fbac0f7981e27ae1234b5dd2be639
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-11-19 21:30:56 +0500

    Использование бибиотеки flag

--- main.go
+++ main.go
@@ -2,16 +2,33 @@ package main
 
 import (
 	"fmt"
-	"os"
+	// "os"
+	"flag"
 )
 
 func main() {
-	argCount := len(os.Args)
-	fmt.Printf("Количество элементов %d\n", argCount)
-	fmt.Println("Аргументы: ", os.Args)
+	// argCount := len(os.Args)
+	// fmt.Printf("Количество элементов %d\n", argCount)
+	// fmt.Println("Аргументы: ", os.Args)
 
-	for _, value := range os.Args {
-		fmt.Printf(" %s\n", value)
-	}
+	// for _, value := range os.Args {
+	// 	fmt.Printf(" %s\n", value)
+	// }
+
+	var (
+		name   string
+		age    int
+		height float64
+	)
+
+	flag.StringVar(&name, "name", "John Dow", "Your name")
+	flag.IntVar(&age, "age", 21, "Your age")
+	flag.Float64Var(&height, "height", 180, "Ваш рост (см)")
+
+	flag.Parse()
+
+	fmt.Printf("Name: %s", name)
+	fmt.Printf("Age: %d", age)
+	fmt.Printf("Height: %.1f", height)
 
 }
