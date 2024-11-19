5b90c76950274fc0c0c27b850ad5d3f2f22eb8c6
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-11-19 20:21:25 +0500

    дописал вывод аргументов построчно

--- main.go
+++ main.go
@@ -7,6 +7,11 @@ import (
 
 func main() {
 	argCount := len(os.Args)
-	fmt.Printf("Количество элементов %d/n", argCount)
+	fmt.Printf("Количество элементов %d\n", argCount)
 	fmt.Println("Аргументы: ", os.Args)
+
+	for _, value := range os.Args {
+		fmt.Printf(" %s\n", value)
+	}
+
 }
