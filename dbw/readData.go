7eeb1da1fc4eb194b8672066adf70cb563cdfcc1
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-11-20 17:51:01 +0500

    Подготовки к загрузке данных из sql файла

--- dbw/readData.go
+++ dbw/readData.go
@@ -1,14 +1,64 @@
 package dwb
 
-import "errors"
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"os"
+)
 
+
+// Инициализация структуры для работы с записами
+func init(){
+	type todo_record struct{
+		id,
+		data_of_creations,
+		date_max,
+		todo_text,
+		is_gone,
+		date_of_gone
+	}
+}
 // ЗАГЛУШКА: Читаю данные из БД
 func ReadRec(id_rec int) (int, error) {
 	// Функция читает данные из БД
 	// id ReadRec--1
+
 	if id_rec < 0 {
 		return 0, errors.New(
 			"id ReadRec--1: недопустимый идентификатор записи")
 	}
+	db_connect, err := sql.Open("sqlite3", "ep20231204_todo_cli.db")
+	// id ReadRec--2:
+	if err != nil {
+		fmt.Printf("ReadRec--2: Не могу открыть БД %v", err)
+		fmt.Scanln()
+		os.Exit(1)
+	}
+	defer db_connect.Close()
+	query := `
+		SELECT 
+			id,
+			data_of_creations,
+			date_max,
+			todo_text,
+			is_gone,
+			date_of_gone
+		from my_todo_list
+	`
+	rows, err := db_connect.Query(query)
+	// id ReadRec--3
+	if err != nil {
+
+		fmt.Printf("ReadRec--3: Не могу прочитать из БД %v", err)
+		fmt.Scanln()
+		os.Exit(1)
+	}
+	defer rows.Close()
+	
+	fmt.Printf("rows type is %T/n", rows)
+
+
+
 	return id_rec, nil
 }
